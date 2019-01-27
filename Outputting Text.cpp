6fbb9b4727f7c8e8442e81e2eb6fcbd2f623a6b9
Jacob Dowdy <jacobdowdy@rocketmail.com>
2019-01-27 14:10:21 -0500

    Update Outputting Text.cpp

--- Outputting Text.cpp
+++ Outputting Text.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 int main() {
+	cout << "Starting program..." << flush;
+
 	cout << "This is some text." << endl;
+
+	cout << "Banana. " << "Apple. " << "Orange. " << endl;
 
 	cout << "This is some more text." << endl;
 
